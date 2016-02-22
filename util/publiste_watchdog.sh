bb99c6320b44885ec0efe9e78061ab6b7a40613b
Piotr <piotr.rygielski@uni-wuerzburg.de>
2016-02-22 15:36:26 +0100

    bullet-proof the watchdog

--- util/publiste_watchdog.sh
+++ util/publiste_watchdog.sh
@@ -4,19 +4,18 @@ echo `date`
 STATUS=`/home/piotr/perl/publiste3/util/test_server_alive.sh`
 STATUS_OK=`/home/piotr/perl/publiste3/util/test_server_OK.sh`
 
-if [ $STATUS -ne 200 ]; then
-	echo "Killing Hypnotoad, status was 404 or 500"
-	#/home/piotr/perl/publiste3/kill_hypnotoad.sh
-	pkill -9 -f /home/piotr/perl/git_publiste/script/hex64-publications
-	/usr/bin/hypnotoad /home/piotr/perl/publiste3/script/hex64-publications
-fi
-echo "Server status (alive) was: $STATUS" 
-echo "Testing status (OK)." 
+RESTART=false
 
+if [ $STATUS -ne 200 ]; then
+	$RESTART=true
+fi
 if [ $STATUS_OK -ne 200 ]; then
-    echo "Killing Hypnotoad, status was 404 or 500"
-    #/home/piotr/perl/publiste3/kill_hypnotoad.sh
+    $RESTART=true
+fi
+
+if [ "$RESTART" = true ]; then
     pkill -9 -f /home/piotr/perl/git_publiste/script/hex64-publications
     /usr/bin/hypnotoad /home/piotr/perl/publiste3/script/hex64-publications
 fi
-echo "Server STATUS_OK was: $STATUS_OK" 
+
+echo "Server status RUNNING/FUNCTIONING was: $STATUS / $STATUS_OK" 
