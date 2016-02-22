c47cd608d4e86f21568b782c4d89a9727e0baf3e
Piotr <piotr.rygielski@uni-wuerzburg.de>
2016-02-22 15:16:16 +0100

    removing obsolete scripts

--- util/deprecated/upload-wue-win2005-rsync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SRV=piotr@132.187.10.5
-DIR=/home/piotr/perl/publiste2
-
-
-# WOW!!
-rsync -rav --exclude '*.db' --exclude 'backups/*' --exclude 'log/*' --exclude 'public/uploads/*' ./* $SRV:$DIR/
