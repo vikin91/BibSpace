522f156014c1618ca4b47adb9a8510157d0fc072
vikin91 <piotr@linux.pl>
2016-05-31 19:47:17 +0200

    add https links to be checked by utils

--- util/test_server_alive.sh
+++ util/test_server_alive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-response=$(curl --write-out %{http_code} --silent --output /dev/null http://se2.informatik.uni-wuerzburg.de/pa/ly/p?team=SE-WUERZBURG)
+response=$(curl --write-out %{http_code} --silent -k --output /dev/null https://se2.informatik.uni-wuerzburg.de/pa/ly/p?team=SE-WUERZBURG)
 echo $response
