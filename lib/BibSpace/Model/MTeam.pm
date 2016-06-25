fa15ffc9d6ad48486b86dd53e83ef8f735652e77
vikin91 <piotr@linux.pl>
2016-06-25 22:54:39 +0200

    fix bugs and write some test for MTeam

--- lib/BibSpace/Model/MTeam.pm
+++ lib/BibSpace/Model/MTeam.pm
@@ -1,165 +1,157 @@
 package MTeam;
-    use Data::Dumper;
-    use utf8;
-    use Text::BibTeX; # parsing bib files
-    use 5.010; #because of ~~ and say
-    use DBI;
-    use Moose;
+use Data::Dumper;
+use utf8;
+use Text::BibTeX;    # parsing bib files
+use 5.010;           #because of ~~ and say
+use DBI;
+use Moose;
 
-   has 'id' => (is => 'rw'); 
-   has 'name' => (is => 'rw'); 
-   has 'parent' => (is => 'rw');
+has 'id'     => ( is => 'rw' );
+has 'name'   => ( is => 'rw' );
+has 'parent' => ( is => 'rw' );
 
 ####################################################################################
 sub static_all {
-  my $self = shift;
-  my $dbh = shift;
+    my $self = shift;
+    my $dbh  = shift;
 
-  my $qry = "SELECT id,
+    my $qry = "SELECT id,
             name,
             parent
         FROM Team";
-  my @objs;
-  my $sth = $dbh->prepare( $qry );
-  $sth->execute();
+    my @objs;
+    my $sth = $dbh->prepare($qry);
+    $sth->execute();
 
-  while(my $row = $sth->fetchrow_hashref()) {
-    my $obj = MTeam->new(
-                          id => $row->{id},
-                          name => $row->{name},
-                          parent => $row->{parent}
-                    );
-    push @objs, $obj;
-  }
-  return @objs;
+    while ( my $row = $sth->fetchrow_hashref() ) {
+        my $obj = MTeam->new(
+            id     => $row->{id},
+            name   => $row->{name},
+            parent => $row->{parent}
+        );
+        push @objs, $obj;
+    }
+    return @objs;
 }
 ####################################################################################
 sub static_get {
-  my $self = shift;
-  my $dbh = shift;
-  my $id = shift;
+    my $self = shift;
+    my $dbh  = shift;
+    my $id   = shift;
 
-  my $qry = "SELECT id,
+    my $qry = "SELECT id,
                     name,
                     parent
           FROM Team
           WHERE id = ?";
 
-  my $sth = $dbh->prepare( $qry );
-  $sth->execute($id);
-  my $row = $sth->fetchrow_hashref();
+    my $sth = $dbh->prepare($qry);
+    $sth->execute($id);
+    my $row = $sth->fetchrow_hashref();
 
-  if(!defined $row){
-    return undef;
-  }
+    if ( !defined $row ) {
+        return undef;
+    }
 
-  my $e = MTeam->new();
-  $e->{id} = $id;
-  $e->{name} = $row->{name};
-  $e->{parent} = $row->{parent};
-  return $e;
+    my $e = MTeam->new();
+    $e->{id}     = $id;
+    $e->{name}   = $row->{name};
+    $e->{parent} = $row->{parent};
+    return $e;
 }
 ####################################################################################
 sub update {
-  my $self = shift;
-  my $dbh = shift;
+    my $self = shift;
+    my $dbh  = shift;
 
-  my $result = "";
+    my $result = "";
 
+    if ( !defined $self->{id} ) {
+        say
+"Cannot update. MTeam id not set. The entry may not exist in the DB. Returning -1";
+        return -1;
+    }
 
-  if(!defined $self->{id}){
-      say "Cannot update. MTeam id not set. The entry may not exist in the DB. Returning -1";
-      return -1;
-  }
-
-  my $qry = "UPDATE Team SET
+    my $qry = "UPDATE Team SET
                 name=?,
                 parent=?
             WHERE id = ?";
-  my $sth = $dbh->prepare( $qry );
-  $result = $sth->execute(
-            $self->{name},
-            $self->{parent},
-            $self->{id}
-            );
-  $sth->finish();
-  return $result;
+    my $sth = $dbh->prepare($qry);
+    $result = $sth->execute( $self->{name}, $self->{parent}, $self->{id} );
+    $sth->finish();
+    return $result;
 }
 ####################################################################################
 sub insert {
-  my $self = shift;
-  my $dbh = shift;
+    my $self = shift;
+    my $dbh  = shift;
+    my $result = "";
 
-  my $result = "";
-
-
-  my $qry = "
-    INSERT INTO Team(
-    name,
-    parent
-    ) 
-    VALUES (?,?);";
-    my $sth = $dbh->prepare( $qry );
-    $result = $sth->execute(
-            $self->{name},
-            $self->{parent},
-            );
-  my $inserted_id = $dbh->last_insert_id('', '', 'Team', '');
-  $self->{id} = $inserted_id;
-  # say "MTeam insert. inserted_id = $inserted_id";
-  $sth->finish();
-  return $inserted_id; #or $result;
+    my $qry = "
+        INSERT INTO Team(
+        name,
+        parent
+        ) 
+        VALUES (?,?);";
+    my $sth = $dbh->prepare($qry);
+    $result = $sth->execute( $self->{name}, $self->{parent}, );
+    $self->{id} = $dbh->last_insert_id( '', '', 'Team', '' );
+    $sth->finish();
+    return $self->{id};
 }
 ####################################################################################
 sub save {
-  my $self = shift;
-  my $dbh = shift;
+    my $self = shift;
+    my $dbh  = shift;
 
-  my $result = "";
+    warn "No databse handle supplied!" unless defined $dbh;
 
+    my $result = "";
 
-  if(!defined $self->{id} or $self->{id} <= 0){
-    my $inserted_id = $self->insert($dbh);
-    $self->{id} = $inserted_id;
-    # say "MTeam save: inserting. inserted_id = ".$self->{id};
-    return $inserted_id;
-  }
-  elsif(defined $self->{id} and $self->{id} > 0){
-    # say "MTeam save: updating ID = ".$self->{id};
-    return $self->update($dbh);
-  }
-  else{
-    warn "MTeam save: cannot either insert nor update :( ID = ".$self->{id};
-  }
+    if ( defined $self->{id} and $self->{id} > 0 ) {
+
+        # say "MTeam save: updating ID = ".$self->{id};
+        return $self->update($dbh);
+    }
+    elsif(defined $self and !defined $self->{name}){
+        warn "Cannot save MTeam that has no name";
+        return -1;
+    }
+    else{
+        my $inserted_id = $self->insert($dbh);
+        $self->{id} = $inserted_id;
+
+        # say "MTeam save: inserting. inserted_id = ".$self->{id};
+        return $inserted_id;
+    }
 }
 ####################################################################################
 sub delete {
-  my $self = shift;
-  my $dbh = shift;
+    my $self = shift;
+    my $dbh  = shift;
 
+    my $qry    = "DELETE FROM Team WHERE id=?;";
+    my $sth    = $dbh->prepare($qry);
+    my $result = $sth->execute( $self->{id} );
+    $self->{id} = undef;
 
-  my $qry = "DELETE FROM Team WHERE id=?;";
-  my $sth = $dbh->prepare( $qry );
-  my $result = $sth->execute($self->{id});
-  $self->{id} = undef;
-
-  return $result;
+    return $result;
 }
 ####################################################################################
-sub static_get_by_name{
-  my $self = shift;
-  my $dbh = shift;
-  my $name = shift;
+sub static_get_by_name {
+    my $self = shift;
+    my $dbh  = shift;
+    my $name = shift;
 
-  my $sth = $dbh->prepare( "SELECT id FROM Team WHERE name=?" );     
-  $sth->execute($name);
-  my $row = $sth->fetchrow_hashref();
-  my $id = $row->{id} || -1;
+    my $sth = $dbh->prepare("SELECT id FROM Team WHERE name=?");
+    $sth->execute($name);
+    my $row = $sth->fetchrow_hashref();
+    my $id = $row->{id} || -1;
 
-  if($id > 0){
-    return MTeam->static_get($dbh, $id);
-  }
-  return undef;
+    if ( $id > 0 ) {
+        return MTeam->static_get( $dbh, $id );
+    }
+    return undef;
 }
 ####################################################################################
 1;
--- t/101-MTeam.t
+++ t/101-MTeam.t
@@ -3,9 +3,8 @@ use Test::More;
 use Test::Mojo;
 
 my $t_anyone = Test::Mojo->new('BibSpace');
-my $self = $t_anyone->app;
-my $dbh = $t_anyone->app->db;
-
+my $self     = $t_anyone->app;
+my $dbh      = $t_anyone->app->db;
 
 use BibSpace::Model::MTeam;
 
@@ -14,31 +13,63 @@ use BibSpace::Controller::Core;
 use Set::Scalar;
 use Data::Dumper;
 
+####################################################################
+subtest 'MTeam basics' => sub {
 
+    $dbh->do('DELETE FROM Team;');
 
-$dbh->do('DELETE FROM Team;');
+    # my $en = MTeam->new();
+    my @teams     = MTeam->static_all($dbh);
+    my $num_teams = scalar(@teams);
+    is( $num_teams, 0, "Got 0 teams" );
 
-# my $en = MTeam->new();
-my @teams = MTeam->static_all($dbh);
-my $num_teams = scalar(@teams);
-is($num_teams, 0, "Got 0 teams");
+    my $t1 = MTeam->new();
+    $t1->{name}   = 'Shy_writers';
+    $t1->{parent} = undef;
+    $t1->save($dbh);
 
+    @teams     = MTeam->static_all($dbh);
+    $num_teams = scalar(@teams);
+    is( $num_teams, 1, "Got 1 teams" );
 
-my $t1 = MTeam->new();
-$t1->{name} = 'Shy_writers';
-$t1->{parent} = undef;
-$t1->save($dbh);
+    my $some_team = shift @teams;
+    is( $some_team->delete($dbh), 1 );
 
-@teams = MTeam->static_all($dbh);
-$num_teams = scalar(@teams);
-is($num_teams, 1, "Got 1 teams");
+    @teams     = MTeam->static_all($dbh);
+    $num_teams = scalar(@teams);
+    is( $num_teams, 0, "Got 0 teams" );
+    is( $some_team->{id}, undef );
 
-my $some_team = shift @teams;
-is($some_team->delete($dbh), 1);
-@teams = MTeam->static_all($dbh);
-$num_teams = scalar(@teams);
-is($num_teams, 0, "Got 0 teams");
-is($some_team->{id}, undef);
+    foreach ( 1 .. 10 ) {
+        my $t2 = MTeam->new();
+        $t2->{name}   = random_string(8);
+        $t2->{parent} = undef;
+        $t2->save($dbh);
+    }
 
+    @teams     = MTeam->static_all($dbh);
+    $num_teams = scalar(@teams);
+    is( $num_teams, 10, "Got 10 teams" );
+};
+
+####################################################################
+subtest 'MTeam basics 2' => sub {
+
+    # test updating
+    my @teams = MTeam->static_all($dbh);
+    my $rteam = $teams[ rand @teams ];
+    $rteam->{name} .= "super";
+    is( $rteam->save($dbh), 1, "updated 1 team" );
+
+    #test getting defined team by id
+    my $the_same = MTeam->static_get( $dbh, $rteam->{id} );
+    is( $the_same->{id}, $rteam->{id}, "getter team for id" );
+
+    #test saving bad team
+    my $bad_team = MTeam->new();
+    $bad_team->{id} = undef;
+    is( $bad_team->save($dbh), -1, "saving bad team" );
+
+};
 
 done_testing();
