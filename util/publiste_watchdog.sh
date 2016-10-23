fa446b83d3ef3b3c6e897a29f193451be319396a
vikin91 <piotr@informatik.uni-wuerzburg.de>
2016-10-23 10:34:07 +0200

    extend watchdog script

--- util/publiste_watchdog.sh
+++ util/publiste_watchdog.sh
@@ -5,10 +5,10 @@ STATUS=`$DIR/util/test_server_alive.sh`
 STATUS_OK=`$DIR/util/test_server_OK.sh`
 
 RESTART=false
-if [ $STATUS -gt 399 ]; then
+if [ $STATUS -gt 399 -o $STATUS -eq 0 ]; then
         RESTART=true
 fi
-if [ $STATUS_OK -gt 399 ]; then
+if [ $STATUS_OK -gt 399 -o $STATUS_OK -eq 0 ]; then
     RESTART=true
 fi
 
