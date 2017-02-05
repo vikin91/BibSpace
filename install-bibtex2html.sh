b09ec132e834d236844c8cad5cc6442c5d084ef1
vikin91 <piotr@linux.pl>
2017-02-05 19:23:46 +0100

    try fix travis problems with bibtex2html

--- install-bibtex2html.sh
+++ install-bibtex2html.sh
@@ -3,3 +3,4 @@ set -e
 wget --no-check-certificate http://www.lri.fr/~filliatr/ftp/bibtex2html/bibtex2html-1.98.tar.gz
 tar -xzvf bibtex2html-1.98.tar.gz
 cd bibtex2html-1.98 && ./configure --prefix=$HOME/bibtex2html && make && make install
+export PATH=$PATH:$HOME/bibtex2html:$HOME/bibtex2html/bin
